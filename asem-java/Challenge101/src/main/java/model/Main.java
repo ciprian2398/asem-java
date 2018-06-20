50bfb22e7378c1325255056916867a4e04286b6e
Nicuta Ciprian <ciprian2398@gmail.com>
2018-06-20 11:07:17 +0300

    corrected by ading 0. added 10 in 10 iteration

--- asem-java/Challenge101/src/main/java/model/Main.java
+++ asem-java/Challenge101/src/main/java/model/Main.java
@@ -1,11 +1,10 @@
 package model;
 
-import java.math.BigInteger;
 import java.util.regex.Pattern;
 
 public class Main {
-    private final int minN = (int) Math.sqrt(10203040506070809L); //101010101
-    private final int maxN = (int) Math.sqrt(19293949596979899L); //138902662
+    private final int minN = (int) Math.sqrt(1020304050607080900L);
+    private final int maxN = (int) Math.sqrt(1929394959697989990L);
     private Pattern pattern = Pattern.compile("1[0-9]2[0-9]3[0-9]4[0-9]5[0-9]6[0-9]7[0-9]8[0-9]9[0-9]0");
 
     public static void main(String[] args) {
@@ -13,17 +12,15 @@ public class Main {
     }
 
     private void job() {
-        for (int n = maxN; n > minN; n--) {
-            if (isOKSquare(n * 10)) {
-                System.out.println(n + " " + BigInteger.valueOf((n*10) * (n*10)).toString());
+        long maxRound10 = maxN + (10 - (maxN % 10));
+        for (long n = maxRound10; n > minN; n -= 10)
+            if (isOKSquare(n)) {
+                System.out.println(n + " " + n * n);
                 return;
             }
-        }
-        // solution: 138901917
     }
 
     private boolean isOKSquare(long n) {
-        BigInteger bi = BigInteger.valueOf(n * n);
-        return pattern.matcher(bi.toString()).matches();
+        return pattern.matcher(String.valueOf(n * n)).matches();
     }
 }
